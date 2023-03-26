e3d4f5284a9ea68fb4a33b7aa5732c84aeac0216
GonzaloEspinoza <gonis1990@gmail.com>
2023-03-26 12:12:33 -0400

    config(release): added hash router in project

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from './App'
 import './index.css';
 
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App></App>
-  </BrowserRouter>
+  </HashRouter>
 );
